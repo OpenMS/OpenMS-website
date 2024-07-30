824422f51f6fd69833a0b499758a6a3273d36144
hjn0415 <hjn0415a@gmail.com>
2024-07-30 10:29:37 +0900

    design: resize slider images

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -17,16 +17,57 @@ $(document).ready(function(){
     },
 
     breakpoints: {
-      720: {
+      800: {
         slidesPerView: 1
       },
-      820: {
+      1024: {
         slidesPerView: 2
       },
-      1024: {
+      1260: {
         slidesPerView: 4
       }
     }
   });
+
+  const swiperSlides = document.querySelectorAll('.swiper-slide');
+  
+  /*swiperSlides.forEach(slide => {
+      const paragraphs = slide.querySelectorAll('p'); 
+      slide.addEventListener('mouseover', () => {
+          slide.style.height = '90%';
+          paragraphs.forEach(p => {
+             p.style.display = 'block';
+          });
+      });
+      slide.addEventListener('mouseout', () => {
+          slide.style.height = '65%';
+          paragraphs.forEach(p => {
+             p.style.display = 'none';
+          });
+       
+      });
+  });*/
+   swiperSlides.forEach(slide => {
+        const paragraphs = slide.querySelectorAll('p');
+
+        slide.addEventListener('mouseover', () => {
+            slide.style.height = '90%';
+        });
+
+        slide.addEventListener('transitionend', (event) => {
+            if (event.propertyName === 'height' && slide.style.height === '90%') {
+                paragraphs.forEach(p => {
+                    p.style.display = 'block';
+                });
+            }
+        });
+
+        slide.addEventListener('mouseout', () => {
+            slide.style.height = '65%';
+            paragraphs.forEach(p => {
+                p.style.display = 'none';
+            });
+        });
+    });
 
 })
