207c785a23ec96def47bff032a0609c426249fe2
Samuel Wein <poshul@gmail.com>
2025-07-02 13:16:14 +0200

    Update addSlash.js
    Converts addSlash to an async function and add a timeout and error handling so that we don't just get a generic error is abibuilder isn't available.

--- .netlify/functions/addSlash.js
+++ .netlify/functions/addSlash.js
@@ -1,32 +1,45 @@
 const fetch = require("node-fetch");
 
-exports.handler = function (event, context, callback) {
-  if (event.path.endsWith("documentation/") || event.path.endsWith("current_doxygen/")) {
-    // Return the page root
-    fetch("https://abibuilder.cs.uni-tuebingen.de/archive/openms/Documentation/release/latest/")
-      .then((response) => response.text())
-      .then((body) => {
-        callback(null, {
-          statusCode: 200,
-          body,
-        });
-      });
-  } else if (event.path.endsWith("develop_doxygen/")) {
-    // Return the page root
-    fetch("https://abibuilder.cs.uni-tuebingen.de/archive/openms/Documentation/nightly/")
-    .then((response) => response.text())
-    .then((body) => {
-        callback(null, {
+exports.handler = async function (event) {
+  try {
+    if (
+      event.path.endsWith("documentation/") ||
+      event.path.endsWith("current_doxygen/")
+    ) {
+      // Return the page root
+      const response = await fetch(
+        "https://abibuilder.cs.uni-tuebingen.de/archive/openms/Documentation/release/latest/",
+        { timeout: 5000 } 
+      );
+      const body = await response.text();
+      return {
         statusCode: 200,
         body,
-        });
-    });
-  } else {
-    // Return a redirect to /page/
-    const location = event.path + "/";
-    callback(null, {
-      statusCode: 301,
-      headers: { location },
-    });
+      };
+    } else if (event.path.endsWith("develop_doxygen/")) {
+      // Return the page root
+      const response = await fetch(
+        "https://abibuilder.cs.uni-tuebingen.de/archive/openms/Documentation/nightly/",
+        { timeout: 5000 }
+      );
+      const body = await response.text();
+      return {
+        statusCode: 200,
+        body,
+      };
+    } else {
+      return {
+        statusCode: 301,
+        headers: {
+          location: event.path + "/",
+        },
+      };
+    }
+  } catch (err) {
+    console.error("AddSlash function error: err.message");
+    return {
+      statusCode: 504,
+      body: "ABIBuilder access timed out or failed"
+    };
   }
 };
